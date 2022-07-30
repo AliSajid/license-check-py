bd6575c108dfcda918abb137b8ad856d164ea16c
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2022-07-30 15:42:17 +0500

    Added the license checker function

--- handler.py
+++ handler.py
@@ -1,6 +1,8 @@
 import datetime
 import logging
 import requests
+from bs4 import BeautifulSoup
+from os import environ
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -14,10 +16,28 @@ def run(event, context):
 
 def check_license(event, context):
     url = "https://dlims.punjab.gov.pk/track/"
+    cnic = environ.get('CNIC', "0000000000000")
 
-    response = requests.get(url)
+    payload = { "trackbynumber": cnic }
+
+    response = requests.post(url, data = payload)
+
     if response.status_code == 200:
-        logger.info("License is valid")
+        soup = BeautifulSoup(response.text, 'html.parser')
+        elem = soup.select("#content > div.big-title.text-center.animated.fadeInDown.delay-01 > h1")
+        if elem:
+            status = elem[0].string
+            logger.info(f"Response from website: {elem[0].string}")
+            if status == "Your regular license is in printing queue.":
+                logger.info("Your license is in printing queue")
+                return True
+            else:
+                logger.info("Your license is not in printing queue")
+                return True
+
+        else:
+            logger.error(f"No response from website")
+        return False
     else:
-        logger.info("License is invalid")
+        logger.error("Call to check license failed")
         return False
