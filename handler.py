c54b8311b1e169708b8ae661c81e9e2314dd84a9
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2022-07-31 03:45:16 +0500

    Removed the dummy check-ip function

--- handler.py
+++ handler.py
@@ -9,14 +9,6 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def run(event, context):
-    current_time = datetime.datetime.now().time()
-    name = context.function_name
-    ip_address = requests.get('http://checkip.amazonaws.com').text
-    logger.info("Your cron function " + name + " ran at " +
-                str(current_time) + " from " + ip_address)
-
-
 def check_license(event, context):
     url = "https://dlims.punjab.gov.pk/track/"
     cnic = environ.get('CNIC', "0000000000000")
