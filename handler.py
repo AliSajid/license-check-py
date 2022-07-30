ba4d6a9f2a67083ea968bd3bf5be5c173af6c112
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2022-07-30 15:14:52 +0500

    Added the check_license function

--- handler.py
+++ handler.py
@@ -11,3 +11,13 @@ def run(event, context):
     name = context.function_name
     ip_address = requests.get('http://checkip.amazonaws.com').text
     logger.info("Your cron function " + name + " ran at " + str(current_time) + " from " + ip_address)
+
+def check_license(event, context):
+    url = "https://dlims.punjab.gov.pk/track/"
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        logger.info("License is valid")
+    else:
+        logger.info("License is invalid")
+        return False
