8171a3c50532cf304d41553fe67cba20db01e7d0
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2022-07-30 22:13:15 +0500

    Added header generation

--- handler.py
+++ handler.py
@@ -3,6 +3,7 @@ import logging
 import requests
 from bs4 import BeautifulSoup
 from os import environ
+from fake_headers import Headers
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -12,19 +13,24 @@ def run(event, context):
     current_time = datetime.datetime.now().time()
     name = context.function_name
     ip_address = requests.get('http://checkip.amazonaws.com').text
-    logger.info("Your cron function " + name + " ran at " + str(current_time) + " from " + ip_address)
+    logger.info("Your cron function " + name + " ran at " +
+                str(current_time) + " from " + ip_address)
+
 
 def check_license(event, context):
     url = "https://dlims.punjab.gov.pk/track/"
     cnic = environ.get('CNIC', "0000000000000")
 
-    payload = { "trackbynumber": cnic }
+    payload = {"trackbynumber": cnic}
 
-    response = requests.post(url, data = payload)
+    headers = Headers(headers=True)
+
+    response = requests.post(url, data=payload, headers=headers.generate())
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
-        elem = soup.select("#content > div.big-title.text-center.animated.fadeInDown.delay-01 > h1")
+        elem = soup.select(
+            "#content > div.big-title.text-center.animated.fadeInDown.delay-01 > h1")
         if elem:
             status = elem[0].string
             logger.info(f"Response from website: {elem[0].string}")
